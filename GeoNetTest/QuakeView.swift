85a4132bbe3d2ba06bc88f668f084361329a80a1
Leandro Martín Peralta <leandro.peralta@powershop.co.nz>
2016-02-19 14:16:25 +1300

    New Quake View.

---
+++ GeoNetTest/QuakeView.swift
@@ -0,0 +1,21 @@
+//
+//  QuakeView.swift
+//  GeoNetTest
+//
+//  Created by Leandro Martín Peralta on 19/02/16.
+//  Copyright © 2016 Matthew Heard. All rights reserved.
+//
+
+import UIKit
+
+class QuakeView: UIView {
+
+    // MARK: Properties
+    @IBOutlet weak var magnitudBigLabel: UILabel!
+    @IBOutlet weak var magnitudeSmallLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var depthLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+}
